f42727fec2a1daa8a4e7a3696e4a658b46dff518
Pranav Daud <pranavdaud@gmail.com>
2025-12-12 23:12:26 +0530

    recording api created

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ const contactRoutes = require("./routes/contactRoutes");
 app.use("/api/contact", contactRoutes);
 
 const videoRoutes = require("./routes/videoRoutes");
-app.use("/api", videoRoutes);
+app.use("/api/video", videoRoutes);
+
+const recordingRoutes = require('./routes/recordingsRoutes');
+ app.use('/api/recordings', recordingRoutes);
+
 
 // Root endpoint
 app.get("/", (req, res) => {
