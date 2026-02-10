55bf703cbdca4f24911ec15fc92bf2affb600055
Pranav Daud <pranavdaud@gmail.com>
2026-02-10 16:47:00 +0530

    10/02/2026 update name and mobile

--- routes/updateName.js
+++ routes/updateName.js
@@ -2,20 +2,52 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// UPDATE FIRST & LAST NAME USING EMAIL
-router.put("/update-name", async (req, res) => {
-  try {
-    const { email, fname, lname } = req.body;
+// // UPDATE FIRST & LAST NAME USING EMAIL
+// router.put("/update-name", async (req, res) => {
+//   try {
+//     const { email, fname, lname } = req.body;
 
-    if (!email || !fname || !lname) {
+//     if (!email || !fname || !lname) {
+//       return res.status(400).json({
+//         message: "Email, first name and last name are required",
+//       });
+//     }
+
+//     const user = await User.findOneAndUpdate(
+//       { email },
+//       { fname, lname },
+//       { new: true }
+//     );
+
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     res.status(200).json({
+//       message: "Name updated successfully",
+//       fname: user.fname,
+//       lname: user.lname,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+
+
+router.put("/update-profile", async (req, res) => {
+  try {
+    const { email, fname, phone } = req.body;
+
+    if (!email || !fname || !phone) {
       return res.status(400).json({
-        message: "Email, first name and last name are required",
+        message: "Email, name and phone are required",
       });
     }
 
     const user = await User.findOneAndUpdate(
       { email },
-      { fname, lname },
+      { fname, phone },
       { new: true }
     );
 
@@ -24,13 +56,12 @@ router.put("/update-name", async (req, res) => {
     }
 
     res.status(200).json({
-      message: "Name updated successfully",
-      fname: user.fname,
-      lname: user.lname,
+      message: "Profile updated successfully",
+      user,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-module.exports = router;
+ module.exports = router;
